a79177fbf93727de34b66203ba2a14bec3ac08bf
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-07 15:10:03 -0600

    Key value pipe and json pipe have been added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ registerLocaleData(es)
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRouterModule,
     SharedModule,
     SalesModule
--- src/app/sales/pages/no-commons/no-commons.component.ts
+++ src/app/sales/pages/no-commons/no-commons.component.ts
@@ -1,16 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-commons',
   templateUrl: './no-commons.component.html',
   styles: [
   ]
 })
-export class NoCommonsComponent implements OnInit {
+export class NoCommonsComponent{
+  // i18nSelect Pipe
+  name: string = 'Susana';
+  genre: string = 'f';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  invitationMap = {
+    'm': 'invitarlo',
+    'f': 'invitarla'
   }
 
+  // i18nPlural Pipe
+  clients = ['Maria', 'Oscar', 'Emely', 'Nancy', 'Maggy'];
+  clientsMap = {
+    '=0': "don't have any clients waiting",
+    '=1': "have one client waiting",
+    '=2': "have 2 clients waiting",
+    'other': "have # clients waiting",
+  }
+
+  // Assignments
+  changePerson(): void{
+    if(this.genre === 'm'){
+      this.name = 'Susana';
+      this.genre = 'f';
+    }else {
+      this.name = 'Oscar';
+      this.genre = 'm'
+    }
+  }
+
+  deleteCustomer():void {
+    this.clients.pop();
+  }
+
+  // KeyValues Pipe
+  person = {
+    name: 'Oscar',
+    age: 25,
+    address: 'Guatemala, Guatemala'
+  }
+
+  // Json Pipe
+  heroes = [
+    {
+      name: 'Superman',
+      fly: true
+    },
+    {
+      name: 'Robbin',
+      fly: false
+    },
+    {
+      name: 'Flash',
+      fly: false
+    },
+  ]
 }
